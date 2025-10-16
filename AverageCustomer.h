ab3332da8b03fc13ad9972144b7fae282f253498
Star-Shaker <norvalmegan05@gmail.com>
2025-10-16 22:40:24 +0200

    Updated ignorant and added other .h

---
+++ AverageCustomer.h
@@ -0,0 +1,18 @@
+#ifndef AVERAGECUSTOMER_H
+#define AVERAGECUSTOMER_H
+
+#include "Customer.h"
+
+class AverageCustomer : public Customer
+{
+private:
+    void showDialog();
+    vector<string> compileOptions();
+    void processChoice(int choice);
+
+public:
+    AverageCustomer();
+    ~AverageCustomer();
+};
+
+#endif
--- IgnorantCustomer.cpp
+++ IgnorantCustomer.cpp
@@ -13,19 +13,18 @@ IgnorantCustomer::~IgnorantCustomer()
 
 void IgnorantCustomer::showDialog()
 {
-    cout << "Customer: \"Hi there! I'm terrible with plants... but I want something nice for my place.\"\n";
-    cout << "Customer: \"I don't get much sunlight in my apartment and I forget to water sometimes.\"\n";
-    cout << "Customer: \"Which plant would you recommend for me?\"\n\n";
+    cout << "Customer: \"Hi there! I want to buy my first plant but I'm terrible with them...\"\n";
+    cout << "Customer: \"My apartment doesn't get much sunlight and I sometimes forget to water.\"\n";
+    cout << "Customer: \"What plant should I buy that I won't kill?\"\n\n";
 }
 
 vector<string> IgnorantCustomer::compileOptions()
 {
     return
     {
-        "A cactus - they're super low maintenance!",
-        "A peace lily - it does well in low light and forgives some neglect.",
-        "A rose bush - it loves full sun and daily watering.",
-        "A bonsai tree - they look fancy and need a lot of care."
+        "Buy a Rose - beautiful flowers that need lots of care",
+        "Buy a Cactus - very low maintenance and hard to kill", 
+        "Buy a Lemon Tree - grows fruit but needs lots of space and care"
     };
 }
 
@@ -34,19 +33,16 @@ void IgnorantCustomer::processChoice(int choice)
     switch (choice)
     {
         case 2:
-            cout << "Customer: \"Oh wow, a peace lily! That sounds perfect for me. Thank you!\"\n";
+            cout << "Customer: \"A cactus! That sounds perfect for me! I can't kill something that needs so little care!\"\n";
             break;
         case 1:
-            cout << "Customer: \"A cactus? But I barely get sunlight... won't it die?\"\n";
+            cout << "Customer: \"Roses need so much care... I'd probably kill it in a week.\"\n";
             break;
         case 3:
-            cout << "Customer: \"That sounds like a lot of work. I think I'll kill it...\"\n";
-            break;
-        case 4:
-            cout << "Customer: \"Oh wow, that sounds hard. Maybe not for me.\"\n";
+            cout << "Customer: \"A lemon tree? That sounds way too advanced for my small apartment.\"\n";
             break;
         default:
-            cout << "Customer: \"I'm not sure I understood that...\"\n";
+            cout << "Customer: \"I'm not sure about that recommendation...\"\n";
             break;
     }
 }
