e2949d05ebbdc92a3f60f5c3ba7ed122709fa4d0
g3rard-j <jordaangerard10@gmail.com>
2025-10-18 14:26:37 +0200

    Doxygen I did on my phone

--- NewGameOption.h
+++ NewGameOption.h
@@ -1,9 +1,17 @@
+/**
+ * @file NewGameOption.h
+ * @brief Header file for NewGameOption class
+ * @author Gerard Jordaan
+ */
 #ifndef NEW_GAME_OPT
 #define NEW_GAME_OPT    
 
 #include "MenuOption.h"
 #include "Game.h"
 
+/**
+ * @class NewGameOption
+ * @brief Ne
 class NewGameOption : public MenuOption {
     private:
         Game* game;
--- PlayerMenu.h
+++ PlayerMenu.h
@@ -1,15 +1,41 @@
+/**
+ * @file PlayerMenu.h
+ * @brief Header file for PlayerMenu class.
+ *  PlayerMenu is the Invoker participant in the 
+ *  command design pattern
+ * @author Gerard Jordaan
+ */
 #ifndef P_MENU
 #define P_MENU
 
 // Forward declaration, ok to do so since using MenuOption*
 class MenuOption;
 
+/**
+ * @class PlayerMenu
+ * @brief Creates object that holds and then executes MenuOption
+ */
 class PlayerMenu {
     private:
+	/**
+	 * @brief Attribute that holds MenuOption reference.
+	 *  set in setMenuOption method
+	 *  used in executeOption to invoke MenuOption execute method
+	 */
         MenuOption* menuOption;
     public:
+	/**
+	 * @brief Setter for menuOption attribute
+	 * @param[in] option MenuOption* reference
+	 */
         void setMenuOption(MenuOption* option);
+	/**
+	 * @brief Invokes execute() method on menuOption attribute
+	 */
         void executeOption();
+	/**
+	 * @brief Frees mem allocated to menuOption attr
+	 */
         ~PlayerMenu();
 };
 
