cd7972d6ab569e777193200f7c1e311aa7bd7a04
g3rard-j <jordaangerard10@gmail.com>
2025-10-18 15:31:10 +0200

    Added logger and abstracted constructor and attributes

--- ContinueGameOption.h
+++ ContinueGameOption.h
@@ -1,16 +1,39 @@
+/**
+ * @file ContinueGameOption.h
+ * @brief Header file for ContinueGameOption class
+ * @author Gerard Jordaan
+ */
+
 #ifndef CONTINUE_GAME_OPT
 #define CONTINUE_GAME_OPT
 
 #include "MenuOption.h"
-#include "Game.h"
 
+/**
+ * @class ContinueGameOption
+ * @brief Inherits from MenuOption. Represents ConcreteCommand participant
+ *  in the Command design pattern. Invokes operation to load a saved game.
+ */
 class ContinueGameOption : public MenuOption {
     private:
-        Game* game;
+        /**
+         * @brief Attribute specific to this class within the Command hierarchy
+         * . GameId used to lookup game data in the database.
+         */
         int gameId;
     public:
-        ContinueGameOption(Game* game, int gameId);
-        virtual void execute();
+        /**
+         * @brief Calls parent constructor and sets gameId attribute
+         * @param[in] game Game object reference
+         * @param[in] gameId GameId of saved game
+         * @param[in] logger Optional reference to Logger object, default is nullptr
+         */
+        ContinueGameOption(Game* game, int gameId, Logger* logger = nullptr);
+        /**
+         * @brief Implements execute method from MenuOption interface. Invokes
+         *  operation on Game object to load an existing game
+         */
+        virtual void execute() override;
 };
 
 #endif // CONTINUE_GAME_OPT
--- NewGameOption.h
+++ NewGameOption.h
@@ -7,17 +7,24 @@
 #define NEW_GAME_OPT    
 
 #include "MenuOption.h"
-#include "Game.h"
 
 /**
  * @class NewGameOption
- * @brief Ne
+ * @brief Implements MenuOption interface. NewGameOption invokes and logs the operation to create a new Game
+ */
 class NewGameOption : public MenuOption {
-    private:
-        Game* game;
     public:
-        NewGameOption(Game* game);
-        virtual void execute();
+        /**
+         * @brief Constructor sets attributes. Calls parent constructor
+         * @param[in] game Game object reference
+         * @param[in] logger Optional reference to Logger object, default is nullptr
+         */
+        NewGameOption(Game* game, Logger* logger = nullptr);
+        /**
+         * @brief Implements execute method from MenuOption interface.
+         *  invokes the method on the Game object related to creating a new game
+         */
+        virtual void execute() override;
 };
 
 #endif // NEW_GAME_OPT
