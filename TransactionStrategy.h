d0938d71ed0defddf328672ac5bd247cd0003db2
Ally-Andre <u23525984@tuks.co.za>
2025-10-18 21:12:53 +0200

    added .h files for strat & mem

---
+++ TransactionStrategy.h
@@ -0,0 +1,13 @@
+#ifndef TRANSACTIONSTRATEGY_H
+#define TRANSACTIONSTRATEGY_H
+
+#include <string>
+
+class TransactionStrategy{
+    public:
+        TransactionStrategy();
+        virtual ~TransactionStrategy()=0;
+        virtual double execute(double v, double b)=0;
+        virtual std::string getType()=0;
+};
+#endif
