3a74e397dbff9dd9f094a8a2dfdc057fbeedc14f
g3rard-j <jordaangerard10@gmail.com>
2025-10-21 16:39:46 +0200

    Logger documentation

--- backend/game/BasicLogger.h
+++ backend/game/BasicLogger.h
@@ -1,6 +1,6 @@
 /**
- * @file Logger.h
- * @brief Header file for Logger class
+ * @file BasicLogger.h
+ * @brief Header file for BasicLogger ConcreteStrategy
  * @author Gerard Jordaan
  */
 
@@ -17,23 +17,40 @@
 
 using namespace std;
 
+/**
+ * @class BasicLogger
+ * @brief Implements Logger interface. Reads LOG_FILE (log file path) environment variable, opens
+ *  and appends log message and timestamp to the file 
+ */
 class BasicLogger : public Logger {
     private:
         /**
-         * @brief Filename of Log File, set in Logger constructor
+         * @brief Filename of Log File, read from environment, LOG_FILE
          */
         string fileName;
+        /**
+         * @brief object representing file to append logs to
+         */
         ofstream file;
     public:
         /**
-         * @brief Loads and sets Log File filename from environment variables
+         * @brief Loads and sets filename from environment variable LOG_FILE
+         *  Opens the file, loads into mem
          */
         BasicLogger();
+        /**
+         * @brief Appends to the log file in the following format:
+         *  [timestamp] <message>
+         */
         void newLog(string message) override;
         /**
-         * @brief Returns current timestamp
+         * @brief Returns current timestamp <weekday month day hh:ss:mm year>
+         * @returns Tue Oct 21 16:37:50 2025
          */
         string timestamp();
+        /**
+         * @brief Closes file
+         */
         ~BasicLogger();
 };
 
--- backend/game/Logger.h
+++ backend/game/Logger.h
@@ -1,10 +1,24 @@
+/**
+ * @file Logger.h
+ * @brief Header for Strategy Logger
+ * @author Gerard Jordaan
+ */
+
 #ifndef LOGGER
 #define LOGGER
 
 #include <string>
 
+/**
+ * @class Logger
+ * @brief Interface for concrete Log strategies to implement
+ */
 class Logger {
     public:
+        /**
+         * @brief Pure virtual function that acts as the algorithm the concrete strategies are to implement
+         * @param[in] message Content of log message
+         */
         virtual void newLog(std::string message) = 0;
 };
 
