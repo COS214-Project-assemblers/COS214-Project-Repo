3c87a2a2dcdb0e6daaaf66eb5e2f83a054b8d080
Star-Shaker <norvalmegan05@gmail.com>
2025-10-21 09:55:24 +0200

    Added Plant.h doxygen

--- backend/plant/Plant.h
+++ backend/plant/Plant.h
@@ -1,3 +1,8 @@
+/**
+ * @file Plant.h
+ * @brief Header file for the Plant class
+ */
+
 #ifndef PLANT_H
 #define PLANT_H
 
@@ -11,33 +16,95 @@ class GreenhouseStaff;
 
 using namespace std;
 
+/**
+ * @class Plant
+ * @brief Abstract class for the Plant objects
+ */
 class Plant
 {
     private:
+        /**
+         * @brief The category of plants available for creation, e.g., "Succulent", "Flower", "Tree"
+         */
         string plantCategory;
+
+        /**
+         * @brief The specific variety of the plant category, e.g., "Daisy", "Cactus", "Lemon"
+         */
         string plantVariety;
+
+        /**
+         * @brief The price that the Plant costs for the player to buy it, 
+         * so that it can be planted in the Greenhouse
+         */
         float costPrice;
+
+        /**
+         * @brief The price that the Customer has to pay to be able to buy the plant from the player's Greenhouse
+         * @details This price is calculated with 50% revenue (can be discussed if it needs to change)
+         */
         float salePrice;
 
         PlantState* plantState;
         string careType;
 
+        /**
+         * @brief A map used for getting the specific costPrice for the variety of the Plant object
+         */
         static map<string, float> plantCosts;
 
         vector<GreenhouseStaff*> observerList;
     
     public:
+        /**
+         * @brief Abstract Plant constructor
+         * @details The specific Plant object is created using the given parameters
+         * @param [in] category The specific category of Plant objects
+         * @param [in] variety The specific variety of the Plant category
+         */
         Plant(string category, string variety);
+
+        /**
+         * @brief Copy constructor
+         * @details This copy constructor is used for the Prototype design pattern to enable cloning 
+         * @param [in] original Reference to the Plant object that is being copied from 
+         */
         Plant(const Plant& original);
+
+        /**
+         * @brief Virtual destructor for polymorphism
+         */
         virtual ~Plant();
         
+        /**
+         * @brief Pure virtual function that enables cloning for the Prototype design pattern, must be implemented by the subclasses
+         */
         virtual Plant* clone() = 0;
 
+        /**
+         * @brief Getter for the Category member variable
+         */
         string getPlantCategory();
+
+        /**
+         * @brief Getter for the Variety member variable
+         */
         string getPlantVariety();
+
+        /**
+         * @brief Getter for the costPrice member variable
+         */
         float getCostPrice();
+
+        /**
+         * @brief Getter for the salePrice member variable
+         */
         float getSalePrice();
 
+        /**
+         * @brief Helper function for testing
+         * @details This function only displays the details about the Plant object to the console, for testing / validation purposes
+         */
         void display();
 
         PlantState* getState();
