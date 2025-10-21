ac06fa76e8b8f806a5ed57a5d7aceeb38d1562a2
Ally-Andre <u23525984@tuks.co.za>
2025-10-21 21:44:51 +0200

    Added doxygen comments and questions/considerations to Customer

--- backend/customer/Customer.h
+++ backend/customer/Customer.h
@@ -1,3 +1,6 @@
+/**
+ * @file Customer.h
+ */
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
@@ -11,22 +14,49 @@ using namespace std;
 class CustomerVisitor;
 class Plant;
 
+/**
+ * @class Customer
+ * @brief Abstract base class defining interface and shared behavior of Customer
+ * 
+ * The Customer class defines the interation methos with the sales floor that is used by all the customer types, such as displaying dialogue, and choosing to make purchases.
+ * 
+ * @see IgnorantCustomer
+ * @see AverageCustomer
+ * @see GreenFingerCustomer
+ */
+
 class Customer
 {
     private:
+        /**
+         * @brief Displays the dialogue from the customer
+         * This is a pure virtual class and mus t be implimented by the derived Customer classes.
+         */
         virtual void showDialog() = 0;
-        virtual vector<string> compileOptions() = 0;
+        virtual vector<string> compileOptions() = 0;//Ally - i dont think we need this as this is done by the manager
+        //Ally - im thinking it could have a random return value that allows it to choose the plant, with a small chance of it being rejected.
         virtual void processChoice(int choice) = 0;
-
     protected:
         int getPlayerChoice(vector<string> options);
         std::string generateRejectionDialogue();
 
     public:
+        /**
+         * @brief Default constructor
+         */
         Customer();
+         /**
+         * @brief virtual deconstructor
+         */
         virtual ~Customer();
         virtual void accept(CustomerVisitor& cV)=0;
+        /**
+         * @brief Allows for customer to evaluate and consider offered plants.
+         * @param offers A vector of pointers to Plant objects of the plants on offer.
+         * @return A pointer to the chosen Plant or NULL if none are chosen.
+         */
         virtual const Plant* considerOptions(const std::vector<const Plant*> offers)const=0;
+        //Ally - I dont think we need this as why would the customer help itself?
         void helpCustomer();
 };
 
