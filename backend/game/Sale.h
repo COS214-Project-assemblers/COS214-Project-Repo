383effdf5f00e2c70c29ac73b150037340fac5db
Ally-Andre <u23525984@tuks.co.za>
2025-10-21 22:46:20 +0200

    Added doxygen comments to Sale

--- backend/game/Sale.h
+++ backend/game/Sale.h
@@ -1,14 +1,37 @@
+/**
+ * @file TransactionStrategy.h
+ */
 #ifndef SALE_H
 #define SALE_H
 
 #include "TransactionStrategy.h"
 #include <string>
 
+/**
+ * @class Sale
+ * @brief
+ */
 class Sale:public TransactionStrategy{
     public:
+        /**
+         * @brief Default constructor
+         */
         Sale();
+        /**
+         * @brief Deconstructor
+         */
         ~Sale();
+         /**
+         * @brief Executes a transactions financial logic to update the balance.
+         * @param v Value of the transaction.
+         * @param b Value of the current balance
+         * @return THe new balance of b+v
+         */
         double execute(double v, double b) override;
+        /**
+         * @brief Returns the Type of the transaction as a Sale
+         * @return "Sale"
+         */
         std::string getType();
 };
 #endif
