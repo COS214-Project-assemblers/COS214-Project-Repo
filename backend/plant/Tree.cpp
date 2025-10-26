578c3ec87c735be48ae93c214e4968d1b7a0fe55
Morgan-Wat <morgz0911@gmail.com>
2025-10-26 13:46:23 +0200

    Added Dynamic Thread Behaviour to the relevant Plant classes

--- backend/plant/Tree.cpp
+++ backend/plant/Tree.cpp
@@ -6,6 +6,10 @@
 #include "Tree.h"
 #include "PlantHealth.h"
 Tree::Tree(string variety) : Plant("Tree", variety) {
+    /**
+     * Different Concrete Plants initalise their Healths' differently so that they
+     * decay at different rates, E.G succulents are watered less than flowers
+     */
     this->health = new Health(2.0, 2.0, 1.0, 0) ;
 }
 
