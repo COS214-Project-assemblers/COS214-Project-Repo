66638c704b9c6d212f12d64816827d61a11d0230
Star-Shaker <norvalmegan05@gmail.com>
2025-10-26 12:51:58 +0200

    Tested plant creation

--- tests/unitTests.cpp
+++ tests/unitTests.cpp
@@ -50,3 +50,66 @@ TEST(GameTests, NewGameExecutesProperly) {
     delete logger;
     // Note: don't delete newGameOption if PlayerMenu takes ownership
 }
+
+TEST(PlantTests, PlantCreation)
+{
+    // Test Succulent creation
+    SucculentCreator succulentCreator;
+    succulentCreator.makePlant("Cactus");
+    
+    EXPECT_TRUE(succulentCreator.hasPlant());
+    
+    Plant* succulent = succulentCreator.getPlant();
+    EXPECT_NE(succulent, nullptr);
+    EXPECT_EQ(succulent->getPlantCategory(), "Succulent");
+    EXPECT_EQ(succulent->getPlantVariety(), "Cactus");
+    
+    // Test cloning succulent
+    Plant* clonedSucculent = succulent->clone();
+    EXPECT_NE(clonedSucculent, nullptr);
+    EXPECT_NE(succulent, clonedSucculent); // Should be different objects
+    EXPECT_EQ(clonedSucculent->getPlantCategory(), "Succulent");
+    EXPECT_EQ(clonedSucculent->getPlantVariety(), "Cactus");
+    
+    delete clonedSucculent;
+
+    // Test Flower creation
+    FlowerCreator flowerCreator;
+    flowerCreator.makePlant("Rose");
+    
+    EXPECT_TRUE(flowerCreator.hasPlant());
+    
+    Plant* flower = flowerCreator.getPlant();
+    EXPECT_NE(flower, nullptr);
+    EXPECT_EQ(flower->getPlantCategory(), "Flower");
+    EXPECT_EQ(flower->getPlantVariety(), "Rose");
+    
+    // Test cloning flower
+    Plant* clonedFlower = flower->clone();
+    EXPECT_NE(clonedFlower, nullptr);
+    EXPECT_NE(flower, clonedFlower);
+    EXPECT_EQ(clonedFlower->getPlantCategory(), "Flower");
+    EXPECT_EQ(clonedFlower->getPlantVariety(), "Rose");
+    
+    delete clonedFlower;
+
+    // Test Tree creation
+    TreeCreator treeCreator;
+    treeCreator.makePlant("Lemon");
+    
+    EXPECT_TRUE(treeCreator.hasPlant());
+    
+    Plant* tree = treeCreator.getPlant();
+    EXPECT_NE(tree, nullptr);
+    EXPECT_EQ(tree->getPlantCategory(), "Tree");
+    EXPECT_EQ(tree->getPlantVariety(), "Lemon");
+    
+    // Test cloning tree
+    Plant* clonedTree = tree->clone();
+    EXPECT_NE(clonedTree, nullptr);
+    EXPECT_NE(tree, clonedTree);
+    EXPECT_EQ(clonedTree->getPlantCategory(), "Tree");
+    EXPECT_EQ(clonedTree->getPlantVariety(), "Lemon");
+    
+    delete clonedTree;
+}
