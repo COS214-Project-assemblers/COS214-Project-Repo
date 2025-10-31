becaaee881377caa3a887ef2ea5e1582772b7541
g3rard-j <jordaangerard10@gmail.com>
2025-10-31 20:04:35 +0200

    Finally got to serve static files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: "./frontend/src/index.js",
@@ -20,7 +21,13 @@ module.exports = {
         {
             test: /\.css$/,
             use: ["style-loader", "css-loader"],
-      },]
+      },
+        {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: 'asset/resource',                 // replaces file-loader
+        generator: {
+          filename: 'assets/[name][contenthash][ext]'  // output path
+        }}]
     },
     resolve: {
             fallback: {
@@ -28,7 +35,21 @@ module.exports = {
               "crypto": false
             }
     },
+    devServer: {
+    static: path.resolve(__dirname, 'frontend', 'dist'),
+    historyApiFallback: true,   // React Router: serve index.html on refresh
+    port: 3000,
+    open: true,
+    },
     plugins: [
-        new NodePolyfillPlugin()
+        new NodePolyfillPlugin(),
+        new CopyPlugin({
+    patterns: [
+        {
+            from: require('path').resolve(__dirname, 'frontend', 'public', 'assets'),
+            to: 'assets'
+        }
+        ]
+    })
     ]
 }
